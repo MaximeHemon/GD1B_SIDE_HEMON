7aaa6f51e62da03b2d2fa7458e26ce23b877114d
MaximeHemon <74669577+MaximeHemon@users.noreply.github.com>
2021-04-01 17:10:54 +0200

    0

---
+++ assets/dune_algue.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="dune_algue" tilewidth="90" tileheight="90" tilecount="2" columns="2">
+ <image source="dune_algue.png" width="180" height="90"/>
+</tileset>
